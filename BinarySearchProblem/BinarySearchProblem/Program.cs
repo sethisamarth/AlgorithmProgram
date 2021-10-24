766c350c2e7618a5e88755355ab125a2693fe3b2
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-24 16:06:00 +0530

    BinarySearch of word from wordlist

---
+++ BinarySearchProblem/BinarySearchProblem/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BinarySearchProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Programs!");
+           
+                Console.WriteLine("1.Binary Search the word from wordlist");
+                Console.WriteLine("Enter the option");
+                int num = Convert.ToInt32(Console.ReadLine());
+                switch (num)
+                {
+                    
+                    case 1:
+                        string txtpath = File.ReadAllText(@"C:\Users\Lenovo\Desktop\Algorithm Programs  problems\BinarySearchProblem\BinarySearchProblem\Algo.txt");
+                        List<string> words = new List<string>(txtpath.Split());
+                        words.Sort();
+                        BinarySearch.BinarySearchWord(words);
+                        break;
+                    default:
+                        Console.WriteLine("Enter the valid option!");
+                        break;
+
+                }
+                Console.WriteLine("\nDo you want to continue");
+            }
+        }
+    }
+
