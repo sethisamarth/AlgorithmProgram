d28c62079b9ccaa47e9d568a2ae685d7d505afaf
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-24 16:41:23 +0530

    Bubble Sort

---
+++ BubbleSortProblem/BubbleSortProblem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BubbleSortProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 10, 2, 14, 3, 1, 5, 9 };
+            BubbleSort.BubbleSortInArray(arr1);
+            BubbleSort.PrintArray(arr1);
+
+        }
+    }
+}
