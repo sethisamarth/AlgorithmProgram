c521e4854b42d88392806b0a36ebb46e07df751a
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-24 16:24:51 +0530

    Insert sort

---
+++ InsertionSortProblem/InsertionSortProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InsertionSortProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            InsertionSort insertionSort = new InsertionSort();
+            int[] arr = { 10, 2, 14, 3, 1, 5, 9 };
+            insertionSort.Sort(arr);
+            insertionSort.PrintArray(arr);
+        }
+    }
+}
